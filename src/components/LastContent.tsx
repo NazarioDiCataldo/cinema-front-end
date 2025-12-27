1523a24d1a78a4e21deb812d4414e2d4a9761115
Nazario Di Cataldo <i3mnan@gmail.com>
2025-12-27 19:04:57 +0100

    feat: add Movie.ts and fix UI/UX of LastContent.tsx component

--- src/components/LastContent.tsx
+++ src/components/LastContent.tsx
@@ -1,66 +1,87 @@
-import { Actor } from "@/lib/Actor";
+import { Actor, type ActorType } from "@/lib/Actor";
 import { Card, CardContent } from "./ui/card";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
+import { Movie, type MovieType } from "@/lib/Movie";
+import { Loader2 } from "lucide-react";
 
 type LastContentProps = {
-    name: 'actors' | 'movies' | 'halls',
-    limit: number,
-    order?: 'ASC' | 'DESC'
-}
+  name: "actors" | "movies" | "halls";
+  limit: number;
+  order?: "ASC" | "DESC";
+};
 
-const LastContent = ({name, limit, order}: LastContentProps) => {
+const LastContent = ({ name, limit, order }: LastContentProps) => {
+  const [list, setList] = useState([]);
 
-    const [list, setList] = useState([]);
+  useEffect(() => {
+    async function getResource() {
+      switch (name) {
+        case "actors":
+          setList(await Actor.get({ limit, order }));
+          break;
 
-    useEffect(() => {
-        async function getResource() {
-            switch(name) {
-                case 'actors': 
-                    setList(await Actor.get({limit, order}));
-                    break;
+        case "movies":
+          setList(await Movie.get({ limit, order }));
+          break;
+      }
+    }
 
-                case 'movies': 
-                    setList(await Actor.get({limit, order}));
-                    break;
-            }
-        };
-
-        getResource();
-    }, [name]);
+    getResource();
+  }, [name]);
 
   return (
     <section className="container mt-8 flex flex-col gap-4">
-        <h2 className="text-primary text-3xl font-semibold">Last {name} loaded</h2>
-      <Carousel
-      opts={{
-        align: "start",
-        loop: true
-      }}
-      className="w-full"
-    >
-      <CarouselContent>
-        {list.map((elem, index) => (
-            <Link to={`/${name}/${elem.id}`} className="flex min-w-0 shrink-0 grow-0 basis-full md:basis-1/2 lg:basis-1/3">
+      <h2 className="text-primary text-3xl font-semibold">
+        Last {name} loaded
+      </h2>
+      {list.length ? (
+        <Carousel
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          className="w-full"
+        >
+          <CarouselContent>
+            {list.map((elem: MovieType | ActorType, index) => (
+              <Link
+                to={`/${name}/${elem.id}`}
+                className="flex min-w-0 shrink-0 grow-0 basis-full md:basis-1/2 lg:basis-1/3"
+              >
                 <CarouselItem key={index}>
-                    <div className="p-1">
+                  <div className="p-1">
                     <Card className="p-0 cursor-pointer">
-                        <CardContent className="flex flex-col aspect-square items-start justify-start p-0">
-                            <picture className="w-full h-64">
-                                <img className="w-full h-full object-cover" src={elem.image_path} />
-                            </picture>
-                        <h3 className="text-xl text-primary font-semibold p-6">{elem.name ?? elem.title}</h3>
-                        </CardContent>
+                      <CardContent className="flex flex-col aspect-square items-start justify-start p-0">
+                        <picture className="w-full h-64">
+                          <img
+                            className="w-full h-full object-cover"
+                            src={elem.image_path}
+                          />
+                        </picture>
+                        <h3 className="text-xl text-primary font-semibold p-6">
+                          {elem.name ?? elem.title}
+                        </h3>
+                      </CardContent>
                     </Card>
-                    </div>
+                  </div>
                 </CarouselItem>
-            </Link>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious className='hidden lg:flex' />
-      <CarouselNext className='hidden lg:flex' />
-    </Carousel>
+              </Link>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="hidden lg:flex" />
+          <CarouselNext className="hidden lg:flex" />
+        </Carousel>
+      ) : (
+        <Loader2 className="animate-spin text-primary size-12" />
+      )}
     </section>
   );
 };
--- src/lib/Movie.ts
+++ src/lib/Movie.ts
@@ -1,26 +1,30 @@
-export type ActorType = {
+export type MovieType = {
   id: number;
-  name: string;
-  birth_year: number;
+  title: string;
+  production_year: number;
   nationality: string;
+  director: string;
+  genre: string;
   image_path: string;
   created_at: string;
   updated_at?: string;
 };
 
-type ActorParams = {
+type MovieParams = {
     limit?: number;
     order?: 'ASC' | 'DESC',
     order_by?: string,
     nationality?: string,
-    birth_year_from?: number,
-    birth_year_to?: number,
-    name?: string  
+    production_year_from?: number,
+    production_year_to?: number,
+    title?: string,
+    director?: string,
+    genre?: string,  
 }
 
-export class Actor {
-  //Get all actors
-  static async get(params: ActorParams = {}) {
+export class Movie {
+  //Get all movies
+  static async get(params: MovieParams = {}) {
 
     const queryParams = [];
 
@@ -34,7 +38,7 @@ export class Actor {
 
     //Verifico se ci sono query params
     //Se ci sono, all'URL aggiungo tutti i query params tramite join
-    const url = queryParams.length ? `${import.meta.env.VITE_ACTORS_URL}?${queryParams.join('&')}` : `${import.meta.env.VITE_ACTORS_URL}`;
+    const url = queryParams.length ? `${import.meta.env.VITE_MOVIES_URL}?${queryParams.join('&')}` : `${import.meta.env.VITE_MOVIES_URL}`;
 
     const res = await fetch(url);
     const resJson = await res.json();
@@ -43,18 +47,18 @@ export class Actor {
   }
 
   static async single(id: string) {
-    const res = await fetch(`${import.meta.env.VITE_ACTORS_URL}/${id}`);
+    const res = await fetch(`${import.meta.env.VITE_MOVIES_URL}/${id}`);
     const resJson = await res.json();
 
     if (!resJson.success) {
-      throw new Error("Actor not found");
+      throw new Error("Movie not found");
     }
 
     return resJson.data;
   }
 
   static async create(data: object) {
-    const res = await fetch(import.meta.env.VITE_ACTORS_URL, {
+    const res = await fetch(import.meta.env.VITE_MOVIES_URL, {
       method: "POST",
       body: JSON.stringify(data),
       headers: { "Content-Type": "application/json" },
@@ -65,7 +69,7 @@ export class Actor {
   }
 
   static async update(id: number, data: object) {
-    const res = await fetch(`${import.meta.env.VITE_ACTORS_URL}/${id}`, {
+    const res = await fetch(`${import.meta.env.VITE_MOVIES_URL}/${id}`, {
       method: "PUT",
       body: JSON.stringify(data),
       headers: { "Content-Type": "application/json" },
@@ -76,7 +80,7 @@ export class Actor {
   }
 
   static async delete(id: number) {
-    const res = await fetch(`${import.meta.env.VITE_ACTORS_URL}/${id}`, {
+    const res = await fetch(`${import.meta.env.VITE_MOVIES_URL}/${id}`, {
       method: "DELETE",
     });
     const resJson = await res.json();
